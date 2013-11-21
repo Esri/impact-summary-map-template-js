9e2f83fba80059f632d66194a910b3ba2efb2c5a
Matt Driscoll <mdriscoll@esri.com>
2013-11-20 17:00:20 -0800

    no message

--- js/Slider.js
+++ js/Slider.js
@@ -29,8 +29,14 @@ function (
             this._i18n = i18n;
             //set no of slide to display
             this.displayPageCount = 3;
+            
+            console.log('test1');
             this._createSlider();
+            
+            console.log('test2');
+            
             topic.subscribe("resizeGeoDataSlider", lang.hitch(this, function(sliderId) {
+                console.log(sliderId);
                 setTimeout(lang.hitch(this, function() {
                     this._resizeSlider(sliderId);
                 }), 0);
@@ -42,7 +48,7 @@ function (
             sliderOuterContainer = domConstruct.create('div', {
                 "id": "slider" + this.sliderContent.id,
                 "class": "divSliderContainer"
-            }, null);
+            });
             sliderInnerContainer = domConstruct.create('div', {
                 "class": "divInnerSliderContainer"
             }, sliderOuterContainer);
@@ -55,9 +61,19 @@ function (
             }, sliderInnerContainer);
             sliderContentDiv = domConstruct.create('div', {
                 "class": "carousel slidePanel"
-            }, null);
-            sliderDiv.appendChild(sliderContentDiv);
-            sliderContentDiv.appendChild(this.sliderContent);
+            });
+            
+            console.log('bleh');
+            
+            
+            domConstruct.place(sliderContentDiv, sliderDiv, "last");
+            //sliderDiv.appendChild(sliderContentDiv);
+            
+            domConstruct.place(this.sliderContent, sliderContentDiv, "last");
+            //sliderContentDiv.appendChild(this.sliderContent);
+            
+            console.log('bleh2');
+            
             sliderRightArrowHolder = domConstruct.create('div', {
                 "class": "divRight"
             }, sliderInnerContainer);
@@ -66,14 +82,27 @@ function (
                 "class": "leftArrow disableArrow",
                 "title": i18n.widgets.Slider.previous
             }, sliderLeftArrowHolder);
+            
+            console.log('bleh3');
+            
             this._createPagination(sliderDiv, sliderOuterContainer);
+            
+            console.log('bleh4');
+            
             sliderDivRightArrow = domConstruct.create('div', {
                 "id": this.sliderContent.id + "rightArrow",
                 "class": "rightArrow",
                 "title": i18n.widgets.Slider.next
             }, sliderRightArrowHolder);
-            this.sliderParent.appendChild(sliderOuterContainer);
+            
+            
+            domConstruct.place(sliderOuterContainer, this.sliderParent, "last");
+            //this.sliderParent.appendChild(sliderOuterContainer);
+            
+            console.log('bleh5');
             this._resizeSlider(sliderOuterContainer.id);
+            
+            console.log('bleh6');
             //change previous/next slide on clicking of left and right arrow.
             on(sliderDivRightArrow, 'click', lang.hitch(this, function() {
                 this._slide(sliderOuterContainer.id, true);
@@ -112,82 +141,167 @@ function (
             }
         },
         _slide: function(sliderId, isSlideRight) {
-            var sliderContent, carousel, newLeft, selectedPage, pageIndex;
+            var sliderContent, carousel, newLeft, selectedPage, pageIndex = 0;
             sliderContent = query('#' + sliderId + ' .divSliderContent')[0];
             carousel = query('#' + sliderId + ' .carousel')[0];
             this._moveSliderPage(sliderId, isSlideRight);
             selectedPage = query('#' + sliderId + ' .bgColor')[0];
-            pageIndex = parseInt(domAttr.get(selectedPage, "index"));
-            newLeft = -(domStyle.get(sliderContent, 'width') + this.displayPageCount) * pageIndex;
-            domStyle.set(carousel, 'left', newLeft + "px");
+            if(selectedPage){
+                pageIndex = parseInt(domAttr.get(selectedPage, "index"));   
+            }
+            
+            console.log(selectedPage);
+            console.log(pageIndex);
+            
+            if(sliderContent){
+                newLeft = -(domStyle.get(sliderContent, 'width') + this.displayPageCount) * pageIndex;
+                domStyle.set(carousel, 'left', newLeft + "px");
+            }
         },
         //set slider panel width on window resize
         _setPanelWidth: function(node) {
             if (node) {
-                var sliderWidth = query('.geoPanel')[0].offsetWidth;
-                domStyle.set(node, 'width', sliderWidth + 'px');
+                var sl = query('.geoPanel');
+                if(sl && sl.length){
+                    var sliderWidth = sl[0].offsetWidth;
+                    domStyle.set(node, 'width', sliderWidth + 'px');
+                }
             }
         },
         //set first page in slider
         _resetSlider: function(sliderId) {
-            var newLeft, carousel = query('#' + sliderId + ' .carousel')[0];
-            newLeft = 0;
-            domStyle.set(carousel, 'left', newLeft + "px");
-            domClass.add(query('#' + sliderId + ' .leftArrow')[0], "disableArrow");
-            domClass.remove(query('#' + sliderId + ' .rightArrow')[0], "disableArrow");
-            domClass.remove(query('#' + sliderId + ' .bgColor')[0], "bgColor");
-            domClass.add(query('#' + sliderId + ' .paginationDot')[0], "bgColor");
+            var carousel = query('#' + sliderId + ' .carousel')[0];
+            if(carousel){
+                domStyle.set(carousel, 'left', 0 + "px");
+            }
+            var disableArrow = query('#' + sliderId + ' .leftArrow')[0];
+            if(disableArrow){
+                domClass.add(disableArrow, "disableArrow");
+            }
+            var rightArrow = query('#' + sliderId + ' .rightArrow')[0];
+            if(rightArrow){
+                domClass.remove(rightArrow, "disableArrow");
+            }
+            var bgColor = query('#' + sliderId + ' .bgColor')[0];
+            if(bgColor){
+                domClass.remove(bgColor, "bgColor");
+            }
+            var paginationDot = query('#' + sliderId + ' .paginationDot')[0];
+            if(paginationDot){
+                domClass.add(paginationDot, "bgColor");
+            }
         },
         //resize slider contents
         _resizeSlider: function(sliderId) {
+            console.log('boo');
             var carousel, sliderContentHolder, sliderTableWidth, selectedPage;
             carousel = query('#' + sliderId + ' .carousel')[0];
+            console.log('boo1');
             sliderContentHolder = query('#' + sliderId + ' .divGeoDataHolder')[0];
-            sliderTableWidth = (domStyle.get(sliderContentHolder.children[0], 'width') + 1) * sliderContentHolder.childElementCount;
-            domStyle.set(sliderContentHolder, 'width', sliderTableWidth + 'px');
-            domStyle.set(carousel, 'width', sliderTableWidth + 'px');
+            if(sliderContentHolder){
+                
+                var geoChildren = query('.data-block', sliderContentHolder);
+                
+                console.log('boo2');
+                if(geoChildren && geoChildren[0]){
+                    console.log('yes');
+                    sliderTableWidth = (domStyle.get(geoChildren[0], 'width') + 1) * geoChildren.length;    
+                }
+                domStyle.set(sliderContentHolder, 'width', sliderTableWidth + 'px');
+            }
+            if(carousel){
+                console.log('boo3');
+                domStyle.set(carousel, 'width', sliderTableWidth + 'px');
+            }
+            console.log('boo4');
             selectedPage = query('#' + sliderId + ' .bgColor')[0];
+            console.log('boo5');
             this._showSelectedPage(sliderId, selectedPage);
+            console.log('boo6');
         },
         //display selected slider page
         _showSelectedPage: function(sliderId, page) {
+            console.log('hah 1');
             var sliderContent, carousel, pageIndex, newLeft;
             sliderContent = query('#' + sliderId + ' .divSliderContent')[0];
+            
+            console.log('hah 2');
             carousel = query('#' + sliderId + ' .carousel')[0];
-            pageIndex = parseInt(domAttr.get(page, "index"));
-            newLeft = -(domStyle.get(sliderContent, 'width') + this.displayPageCount) * pageIndex;
-            domStyle.set(carousel, 'left', newLeft + "px");
-            domClass.remove(query('#' + sliderId + ' .bgColor')[0], "bgColor");
-            domClass.add(page, "bgColor");
+            
+            console.log('hah 3');
+            
+            var pages = query('.paginationDot', sliderContent);
+            
+            console.log(pages);
+            
+            console.log(page);
+            
+            var pageIndex = 0;
+            if(page){
+                pageIndex = parseInt(domAttr.get(page, "index"), 10);    
+            }
+            
+            console.log('hah 4');
+            if(sliderContent){
+                newLeft = -(domStyle.get(sliderContent, 'width') + this.displayPageCount) * pageIndex;
+                console.log('hah 5');
+            }
+            if(carousel){
+                domStyle.set(carousel, 'left', newLeft + "px");
+                console.log('hah 6');
+            }
+            var bgColor = query('#' + sliderId + ' .bgColor')[0];
+            
+            console.log('hah 7');
+            
+            if(bgColor){
+                domClass.remove(bgColor, "bgColor");
+                console.log('hah 8');
+            }
+            if(page){
+                domClass.add(page, "bgColor");    
+            }
+            
+            console.log('hah 9');
         },
         //handle left/right arrow visibility
         _setArrowVisibility: function(sliderId) {
-            var pageId, pageCount, currentPage = query('#' + sliderId + ' .bgColor')[0];
-            pageId = parseInt(domAttr.get(currentPage, "index"));
-            pageCount = query('#' + sliderId + ' .paginationDot').length;
-            if (pageId === 0) {
-                domClass.add(query('#' + sliderId + ' .leftArrow')[0], "disableArrow");
-                domClass.remove(query('#' + sliderId + ' .rightArrow')[0], "disableArrow");
-            } else if (pageId < pageCount - 1) {
-                domClass.remove(query('#' + sliderId + ' .leftArrow')[0], "disableArrow");
-                domClass.remove(query('#' + sliderId + ' .rightArrow')[0], "disableArrow");
-            } else if (pageId === pageCount - 1) {
-                domClass.add(query('#' + sliderId + ' .rightArrow')[0], "disableArrow");
-                domClass.remove(query('#' + sliderId + ' .leftArrow')[0], "disableArrow");
+            var pageId = 0, pageCount, currentPage = query('#' + sliderId + ' .bgColor')[0];
+            if(currentPage){
+                if(currentPage){
+                    pageId = parseInt(domAttr.get(currentPage, "index"));   
+                }
+                pageCount = query('#' + sliderId + ' .paginationDot').length;
+                if (pageId === 0) {
+                    domClass.add(query('#' + sliderId + ' .leftArrow')[0], "disableArrow");
+                    domClass.remove(query('#' + sliderId + ' .rightArrow')[0], "disableArrow");
+                } else if (pageId < pageCount - 1) {
+                    domClass.remove(query('#' + sliderId + ' .leftArrow')[0], "disableArrow");
+                    domClass.remove(query('#' + sliderId + ' .rightArrow')[0], "disableArrow");
+                } else if (pageId === pageCount - 1) {
+                    domClass.add(query('#' + sliderId + ' .rightArrow')[0], "disableArrow");
+                    domClass.remove(query('#' + sliderId + ' .leftArrow')[0], "disableArrow");
+                }   
             }
         },
         //display next/previous slider page
         _moveSliderPage: function(sliderId, isSlideRight) {
-            var nxtPageId, nextPage, currentPage = query('#' + sliderId + ' .bgColor')[0];
-            nxtPageId = parseInt(domAttr.get(currentPage, "index"));
+            var nxtPageId = 0, nextPage, currentPage = query('#' + sliderId + ' .bgColor')[0];
+            if(currentPage){
+                nxtPageId = parseInt(domAttr.get(currentPage, "index"));
+            }
             if (isSlideRight) {
                 nxtPageId++;
             } else {
                 nxtPageId--;
             }
             nextPage = query('#' + sliderId + ' .paginationDot')[nxtPageId];
-            domClass.remove(currentPage, "bgColor");
-            domClass.add(nextPage, "bgColor");
+            if(currentPage){
+                domClass.remove(currentPage, "bgColor");
+            }
+            if(nextPage){
+                domClass.add(nextPage, "bgColor");
+            }
             this._setArrowVisibility(sliderId);
         }
     });
--- js/main.js
+++ js/main.js
@@ -364,16 +364,17 @@ function(
                 }
                 sum.numFormat = lang.hitch(this, function() {
                     return lang.hitch(this, function(text, render) {
-                        if (render(text).length >= 7) {
+                        var renderedText = render(text);
+                        if (renderedText.length >= 7) {
                             decPlaces = 1;
-                        } else if (render(text).length >= 5) {
+                        } else if (renderedText.length >= 5) {
                             decPlaces = 0;
-                        } else if (render(text).length === 4) {
-                            return number.format(parseInt(render(text), 10));
+                        } else if (renderedText.length === 4) {
+                            return number.format(parseInt(renderedText, 10));
                         } else {
                             decPlaces = 2;
                         }
-                        return this._formatNumber(parseInt(render(text)), decPlaces);
+                        return this._formatNumber(parseInt(renderedText, 10), decPlaces);
                     });
                 });
                 // show geo stats
@@ -392,38 +393,47 @@ function(
                 // remove exiting slider events
                 this._removeSliderEvents();
                 //Create Slider for Geo data panels
-                var slider, objSlider, childNode, divGeoPanel, sliderResizeHandler = null;
+                var sliders, objSlider, childNode, divGeoPanel, sliderResizeHandler = null;
                 // todo
-                slider = query('.' + this.css.statsPanelSelected + ' .' + this.css.divOuterSliderContainer, dom.byId('geodata_container'));
-                // each slider node
-                array.forEach(slider, lang.hitch(this, function(node) {
-                    divGeoPanel = query('.' + this.css.divGeoDataHolder, node)[0];
-                    childNode = query('.' + this.css.statsPanelDataBlock, divGeoPanel).length;
-                    this._setPanelWidth(node.parentElement);
-                    if (childNode > 3) {
-                        if (divGeoPanel) {
-                            objSlider = new Slider({
-                                sliderContent: divGeoPanel,
-                                sliderParent: node
-                            });
-                        }
-                    }
-                    if (!sliderResizeHandler) {
-                        //set slider position on window resize
-                        sliderResizeHandler = on(window, 'resize', lang.hitch(this, function() {
-                            this._setLeftPanelVisibility();
-                            setTimeout(lang.hitch(this, function() {
-                                array.forEach(slider, lang.hitch(this, function(sliderNode) {
-                                    this._setPanelWidth(sliderNode.parentElement);
+                sliders = query('.' + this.css.statsPanelSelected + ' .' + this.css.divOuterSliderContainer, dom.byId('geodata_container'));
+                
+                if(sliders && sliders.length){
+                    // each slider node
+                    array.forEach(sliders, lang.hitch(this, function(node) {
+                        divGeoPanel = query('.' + this.css.divGeoDataHolder, node)[0];
+                        
+                        if(divGeoPanel){
+                            childNode = query('.' + this.css.statsPanelDataBlock, divGeoPanel).length;
+                            this._setPanelWidth(node.parentElement);
+                            if (childNode > 3) {
+                                if (divGeoPanel) {
+                                    objSlider = new Slider({
+                                        sliderContent: divGeoPanel,
+                                        sliderParent: node
+                                    });
+                                }
+                            }
+                            if (!sliderResizeHandler) {
+                                //set slider position on window resize
+                                sliderResizeHandler = on(window, 'resize', lang.hitch(this, function() {
+                                    this._setLeftPanelVisibility();
+                                    setTimeout(lang.hitch(this, function() {
+                                        array.forEach(sliders, lang.hitch(this, function(sliderNode) {
+                                            this._setPanelWidth(sliderNode.parentElement);
+                                        }));
+                                    }), 100);
                                 }));
-                            }), 100);
-                        }));
-                        this._sliderEvents.push(sliderResizeHandler);
-                    }
-                    if (divGeoPanel.lastElementChild) {
-                        domStyle.set(divGeoPanel.lastElementChild, "border", "none");
-                    }
-                }));
+                                this._sliderEvents.push(sliderResizeHandler);
+                            }
+                            var panels = query('.' + this.css.statsPanelDataBlock + ':last-child', divGeoPanel);
+                            if(panels && panels.length){
+                                for(i = 0; i < panels.length; i++){
+                                    domStyle.set(panels[i], 'border', 'none');
+                                }
+                            }
+                        }
+                    }));
+                }
                 if (panelType) {
                     this._showExpanded(panelType);
                 }
@@ -538,28 +548,35 @@ function(
         },
         _showExpanded: function(type) {
             // todo
-            var domSlider, divCount;
-            domSlider = query('.' + this.css.statsPanelSelected + '[data-type="' + type + '"]', this.dataNode)[0];
-            if (domStyle.get(domSlider, 'display') === 'none') {
-                var panels = query('.' + this.css.statsPanelSelected, this.dataNode); 
-                array.forEach(panels, lang.hitch(this, function(elementCount) {
-                    domStyle.set(elementCount, 'display', 'none');
-                    domClass.remove(elementCount, this.css.animateSlider);
-                }));
-                var items = query('.' + this.css.menuPanel, this.dataNode);
-                array.forEach(items, lang.hitch(this, function(elementCount) {
-                    domClass.remove(elementCount, this.css.statsPanelSelectedExpand);
-                }));
-                divCount = query('.' + this.css.statsPanel + ' .' + this.css.statsCount, this.dataNode);
-                //display slider
-                this._displayContainer(domSlider);
-                //hide geo-data count panels.
-                array.forEach(divCount, function(elementCount) {
-                    domStyle.set(elementCount, 'display', 'none');
-                });
-                this._setPanelWidth(domSlider);
-                var selected = query('.' + this.css.menuPanel + '[data-type="' + type + '"]', this.dataNode)[0];
-                domClass.add(selected, this.css.statsPanelSelectedExpand);
+            var sliders, domSlider, divCount;
+            sliders = query('.' + this.css.statsPanelSelected + '[data-type="' + type + '"]', this.dataNode);
+            
+            console.log(sliders);
+            
+            if(sliders && sliders.length){
+                domSlider = sliders[0];
+                console.log(domSlider);
+                if (domStyle.get(domSlider, 'display') === 'none') {
+                    var panels = query('.' + this.css.statsPanelSelected, this.dataNode); 
+                    array.forEach(panels, lang.hitch(this, function(elementCount) {
+                        domStyle.set(elementCount, 'display', 'none');
+                        domClass.remove(elementCount, this.css.animateSlider);
+                    }));
+                    var items = query('.' + this.css.menuPanel, this.dataNode);
+                    array.forEach(items, lang.hitch(this, function(elementCount) {
+                        domClass.remove(elementCount, this.css.statsPanelSelectedExpand);
+                    }));
+                    divCount = query('.' + this.css.statsPanel + ' .' + this.css.statsCount, this.dataNode);
+                    //display slider
+                    this._displayContainer(domSlider);
+                    //hide geo-data count panels.
+                    array.forEach(divCount, function(elementCount) {
+                        domStyle.set(elementCount, 'display', 'none');
+                    });
+                    this._setPanelWidth(domSlider);
+                    var selected = query('.' + this.css.menuPanel + '[data-type="' + type + '"]', this.dataNode)[0];
+                    domClass.add(selected, this.css.statsPanelSelectedExpand);
+                }
             }
         },
         // get layer of impact area by layer title
@@ -586,7 +603,7 @@ function(
             domClass.add(node, this.css.rendererLoading);
             // search query
             var q = new Query();
-            if(value === "summarize" || !value){
+            if(value === "summarize" || value === ""){
                 q.where = '1 = 1';
             }
             else{
