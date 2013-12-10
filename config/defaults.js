20f60f3af29d226e62682350d937b30d5bd805cf
Matt Driscoll <mdriscoll@esri.com>
2013-12-10 11:10:53 -0800

    no message

--- config/defaults.js
+++ config/defaults.js
@@ -5,14 +5,11 @@ define([], function() {
     var defaults = {
         "appid": "",
         "webmap": "8c59dfba1f9b46ca88331f1a830a0ee9",
-        // "0eece0d5de2140e9a44d8050f943fd18", "de5ae0c2040c49d38e9ea0637454ac73"
         "oauthappid": null,
         //"AFTKRmv16wj14N3z",
-        //Group templates must support a group url parameter. This will contain the id of the group.
-        //group: "",
         //Enter the url to the proxy if needed by the applcation. See the "Using the proxy page" help topic for details
         //http://developers.arcgis.com/en/javascript/jshelp/ags_proxy.html
-        "proxyurl": "",
+        "proxyurl": "/resource-proxy/PHP/auth_proxy.php",
         //Example of a template specific property. If your template had several color schemes
         //you could define the default here and setup configuration settings to allow users to choose a different
         //color theme.
