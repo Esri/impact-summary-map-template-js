cf37397d1b46887923d8da501b452237ee0bbcbc
Matt Driscoll <matthew.driscoll@gmail.com>
2014-03-03 11:26:13 -0800

    no message

--- js/main.js
+++ js/main.js
@@ -351,6 +351,15 @@ function(
         _createGeocoderOptions: function() {
             var hasEsri = false,
                 esriIdx, options, geocoders = lang.clone(this.config.helperServices.geocode);
+            //only use geocoders with a url
+            geocoders = array.filter(geocoders, function (geocoder) {
+                if (geocoder.url) {
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            });
             // each geocoder
             array.forEach(geocoders, function (geocoder) {
                 if (geocoder.url.indexOf(".arcgis.com/arcgis/rest/services/World/GeocodeServer") > -1) {
@@ -395,8 +404,15 @@ function(
                     options.geocoders = geocoders;
                 }
             } else {
-                options.arcgisGeocoder = false;
-                options.geocoders = geocoders;
+                if (geocoders.length > 0) {
+                    options.arcgisGeocoder = false;
+                    options.geocoders = geocoders;
+                }
+                else{
+                    options.arcgisGeocoder = true;
+                    options.autoComplete = true;
+                    options.geocoders = null;
+                }
             }
             return options;
         },
