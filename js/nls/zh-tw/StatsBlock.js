82694fd300a33578f1be8a56e166b7f90d217a2b
Matt Driscoll <MDriscoll@esri.com>
2015-06-23 10:26:29 -0700

    NLS

---
+++ js/nls/zh-tw/StatsBlock.js
@@ -0,0 +1,10 @@
+﻿define(
+     ({
+        "StatsBlock": {
+            "close": "關閉",
+            "next": "下一步",
+            "previous": "上一步",
+            "source":"資料來源"
+        }
+    })
+);
